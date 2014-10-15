79eb33e80c823ac3083e4642051c3e8147359c2d
SomaticIT <contact@somatic.fr>
2014-10-15 20:44:56 +0200

    Add tests for azure table

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 ﻿module.exports = function (grunt) {
-
     grunt.initConfig({
         
         //#region Blob
@@ -11,7 +10,7 @@
             },
                 
             simple: {
-                src: 'tests/upload/**',
+            src: 'tests/upload/**',
                 dest: 'TestPrefix/',
                 options: {
                     containerDelete: true,
@@ -104,12 +103,12 @@
             async: {
                 actions: [
                     function (work, callback) {
-                        console.log(work, "waiting, 5 sec");
-                        setTimeout(callback, 5000);
-                    },
+                    console.log(work, "waiting, 5 sec");
+                    setTimeout(callback, 5000);
+                },
                     function (work) {
-                        console.log(work, "sync");
-                    },
+                    console.log(work, "sync");
+                },
                 ]
             },
             list: {
@@ -136,10 +135,123 @@
         },
         
         //#endregion
+        
+        //#region Table
+        
+        "azure-table-insert": {
+            options: {
+                serviceOptions: "UseDevelopmentStorage=true;",
+                table: "testtable"
+            },
+            
+            simple: {
+                options: {
+                    tableDelete: true
+                },
+
+                entity: {
+                    PartitionKey: "simple",
+                    RowKey: "Entity1",
+                    Title: "Entity #1",
+                    Description: "Lorem ipsum"
+                }
+            },
+            
+            multiple: {
+                entities: [
+                    {
+                        PartitionKey: "multiple",
+                        RowKey: "Entity2",
+                        Title: "Entity #2",
+                        Description: "Lorem ipsum"
+                    },
+                    {
+                        PartitionKey: "multiple",
+                        RowKey: "Entity3",
+                        Title: "Entity #3",
+                        Description: "Lorem ipsum"
+                    }
+                ]
+            },
+
+            files: {
+                src: "tests/tableinsert/*.json"
+            }
+        },
+        
+        "azure-table-update": {
+            options: {
+                serviceOptions: "UseDevelopmentStorage=true;",
+                table: "testtable"
+            },
+            
+            simple: {
+                entity: {
+                    PartitionKey: "simple",
+                    RowKey: "Entity1",
+                    Title: "Entity #1",
+                    Description: "Lorem ipsum - updated"
+                }
+            },
+            
+            multiple: {
+                entities: [
+                    {
+                        PartitionKey: "multiple",
+                        RowKey: "Entity2",
+                        Title: "Entity #2",
+                        Description: "Lorem ipsum - updated"
+                    },
+                        {
+                        PartitionKey: "multiple",
+                        RowKey: "Entity3",
+                        Title: "Entity #3",
+                        Description: "Lorem ipsum - updated"
+                    }
+                ]
+            },
+            
+            files: {
+                src: "tests/tableupdate/*.json"
+            }
+        },
+        
+        "azure-table-query": {
+            options: {
+                serviceOptions: "UseDevelopmentStorage=true;",
+                table: "testtable"
+            },
+
+            simple: {
+                actions: function (entities) { console.log(entities); }
+            },
+
+            prefix: {
+                options: {
+                    pkPrefix: "multiple",
+                    raw: true
+                },
+
+                actions: [
+                    function (entities) { console.log(entities); },
+                    function (entities, cb) {
+                        console.log("Waiting 2 seconds");
+                        setTimeout(cb, 2000);
+                    }
+                ]
+            },
+            
+            file: {
+                dest: "tests/tablebackup/backup.json"
+            }
+        },
+
+        //#endregion
 
         clean: {
             download: 'tests/download/**',
-            queue: 'tests/queuebackup/**'
+            queue: 'tests/queuebackup/**',
+            table: 'tests/tablebackup/**',
         }
 
     });
@@ -149,6 +261,7 @@
 
     grunt.registerTask("azure-blob", ["azure-blob-upload", "azure-blob-download"]);
     grunt.registerTask("azure-queue", ["azure-queue-enqueue", "azure-queue-dequeue", "azure-queue-clear"]);
+    grunt.registerTask("azure-table", ["azure-table-insert", "azure-table-update", "azure-table-query"]);
 
-    grunt.registerTask('default', ['clean', 'azure-blob', 'azure-queue']);
+    grunt.registerTask('default', ['clean', 'azure-blob', 'azure-queue', 'azure-table']);
 };
